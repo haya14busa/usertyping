8bdee2cc935d5ecc87a701b3a87144f5ff58186e
haya14busa <hayabusa1419@gmail.com>
2015-07-19 16:39:24 +0900

    Use :username instead of :id in route for users#show

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   resources :texts
   resources :collections
   devise_for :users
-  resources :users, :only => [:show], :path => 'u'
+  get 'u/:username' => 'users#show'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
