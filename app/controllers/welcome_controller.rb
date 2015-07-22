25bb56a351170e3414d9d58c2738f9c7e58182f3
haya14busa <hayabusa1419@gmail.com>
2015-07-23 06:30:35 +0900

    Add sum stats

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,6 +2,8 @@ class WelcomeController < ApplicationController
   before_action :set_newest_collections
 
   def index
+    @n_of_collections = Collection.count
+    @n_of_users = User.count
   end
 
   private
