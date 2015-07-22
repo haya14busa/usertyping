56fb30f930e222c486f0645531eb51325aad6fa4
haya14busa <hayabusa1419@gmail.com>
2015-07-23 05:30:12 +0900

    Add newest collection view

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,2 +1,11 @@
 class WelcomeController < ApplicationController
+  before_action :set_newest_collections
+
+  def index
+  end
+
+  private
+    def set_newest_collections
+      @collections = Collection.order("created_at DESC").limit(10)
+    end
 end
